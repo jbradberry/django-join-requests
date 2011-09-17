c12e7e6af29bdc741b303b8b8230c8fc7a397690
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-09-17 13:34:18 -0400

    Changed the forms to reflect the changes to the JoinRequest model.

--- join/forms.py
+++ join/forms.py
@@ -1,49 +1,14 @@
 from django import forms
 from models import JoinRequest
 
-PENDING = JoinRequest.PENDING
-WITHDRAWN = JoinRequest.WITHDRAWN
-ACCEPTED = JoinRequest.ACCEPTED
-REJECTED = JoinRequest.REJECTED
+
+class JoinHostForm(forms.ModelForm):
+    class Meta:
+        model = JoinRequest
+        fields = ('host_message', 'host_state')
 
 
-class JoinRequestForm(forms.ModelForm):
+class JoinUserForm(forms.ModelForm):
     class Meta:
         model = JoinRequest
         fields = ('message', 'state')
-
-    DISALLOWED = {PENDING: (ACCEPTED, REJECTED),
-                  WITHDRAWN: (ACCEPTED, REJECTED),
-                  ACCEPTED: (PENDING, REJECTED),
-                  REJECTED: (PENDING, WITHDRAWN, ACCEPTED)}
-
-    DISALLOWED_MSG = {PENDING: "Your request has already been processed.",
-                      WITHDRAWN: "You cannot alter a rejected request.",
-                      ACCEPTED: "You cannot accept your own request."
-                      REJECTED: "You cannot reject your own request."}
-
-    def clean_state(self):
-        if self.cleaned_data['state'] in DISALLOWED[self.initial['state']]:
-            raise forms.ValidationError(
-                DISALLOWED_MSG[self.cleaned_data['state']])
-
-
-class JoinResponseForm(forms.ModelForm):
-    class Meta:
-        model = JoinRequest
-        fields = ('response', 'state')
-
-    DISALLOWED = {PENDING: (WITHDRAWN,),
-                  WITHDRAWN: (PENDING, ACCEPTED, REJECTED),
-                  ACCEPTED: (WITHDRAWN,),
-                  REJECTED: (WITHDRAWN,)}
-
-    DISALLOWED_MSG = {PENDING: "You cannot alter a withdrawn request.",
-                      WITHDRAWN: "You cannot withdraw someone else's request.",
-                      ACCEPTED: "You cannot accept a withdrawn request.",
-                      REJECTED: "You cannot reject a withdrawn request."}
-
-    def clean_state(self):
-        if self.cleaned_data['state'] in DISALLOWED[self.initial['state']]:
-            raise forms.ValidationError(
-                DISALLOWED_MSG[self.cleaned_data['state']])
